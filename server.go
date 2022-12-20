7f78a3356fde3b514cdfe24c1095a1a63b54e875
Mateusz Gula <mateuszgua@wp.eu>
2022-12-20 14:52:36 +0100

    Feat: add database connection, add function for save data in database, add test data for save in database

--- server.go
+++ server.go
@@ -5,6 +5,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	mongodb "mateuszgua/to-do-list/database"
+	userData "mateuszgua/to-do-list/database/model"
+
+	"github.com/joho/godotenv"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +28,39 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	godotenv.Load(".env")
+
+	userName := os.Getenv("MONGO_USERNAME")
+	userPassword := os.Getenv("MONGO_PASSWORD")
+	databaseName := os.Getenv("MONGO_DATABASE_NAME")
+	collectionName := os.Getenv("MONGO_COLLECTION_NAME")
+	mongoHost := os.Getenv("MONGO_HOST")
+	mongoServerPort := os.Getenv("MONGO_PORT")
+	authSource := os.Getenv("MONGO_AUTH_SOURCE")
+	authUserName := os.Getenv("MONGO_AUTH_USER")
+	authUserPassword := os.Getenv("MONGO_AUTH_PASSWORD")
+
+	// add collection name for user and for task
+	uri := fmt.Sprintf("mongodb://%s:%s@%s%s", userName, userPassword, mongoHost, mongoServerPort)
+
+	mongoStore, err := mongodb.NewMongoMetaDataStore(uri, authSource, authUserName, authUserPassword, databaseName, collectionName)
+	if err != nil {
+		log.Fatal("failed to create new mongo client", err)
+	}
+
+	currentTime := time.Now()
+
+	testSaveDataInDb := userData.UserMetaData{
+		FirstName:      "Jan",
+		LastName:       "Kowalski",
+		Password:       "Qwerty1234",
+		Email:          "jankowalski@gmail.com",
+		IndexationDate: currentTime,
+	}
+
+	mongoUserId, _ := mongoStore.SaveMetaData(testSaveDataInDb)
+	log.Println(mongoUserId)
+
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/hello", helloHandler)
@@ -34,9 +73,9 @@ func main() {
 	serverUrl := fmt.Sprintf(":%s", httpPort)
 	log.Printf("Starting server on http://localhost%s", serverUrl)
 
-	err := http.ListenAndServe(serverUrl, nil)
+	err = http.ListenAndServe(serverUrl, nil)
 	if err != nil {
-		log.Fatal("culdn't start server", err)
+		log.Fatal("failed to start server", err)
 	}
 
 }
