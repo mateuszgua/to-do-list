be785380033a470cc1d4e5408e18d814fccb3dc0
Mateusz Gula <mateuszgua@wp.eu>
2022-12-21 17:27:15 +0100

    Feat: add login function

--- user/login.go
+++ user/login.go
@@ -7,6 +7,7 @@ import (
 	userData "mateuszgua/to-do-list/database/model"
 	"net/http"
 
+	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -32,4 +33,45 @@ func getHash(pwd []byte) string {
 
 	}
 	return string(hash)
+}
+
+var SECRET_KEY = []byte("gosecretkey")
+
+func userLogin(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	var user userData.UserMetaData
+	var dbUser userData.UserMetaData
+	json.NewDecoder(request.Body).Decode(&user)
+	user, err := mongo.MongoMetaDataStore.GetUserMetaData()
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message":"` + err.Error() + `"}`))
+		return
+	}
+	userPass := []byte(user.Password)
+	dbPass := []byte(dbUser.Password)
+	passErr := bcrypt.CompareHashAndPassword(dbPass, userPass)
+	if passErr != nil {
+		log.Println(passErr)
+		response.Write([]byte(`{"response":"Wrong password!"}`))
+		return
+	}
+
+	jwtToken, err := GenerateJWT()
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message":"` + err.Error() + `"}`))
+		return
+	}
+	response.Write([]byte(`{"token":"` + jwtToken + `"}`))
+}
+
+func GenerateJWT() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	tokenString, err := token.SignedString(SECRET_KEY)
+	if err != nil {
+		log.Println("Error in JWT token generation")
+		return "", err
+	}
+	return tokenString, nil
 }
